5cfe2942ad2d43e17c396da78cf036d6af457a08
Zainab Khatoon <ktn.znb@gmail.com>
2024-11-26 15:41:53 +0400

    Added all files after making the manifest file

---
+++ products.js
@@ -0,0 +1,62 @@
+let products = [
+  {
+    id: 1001,
+    title: "Cat Food",
+    description:
+      "A 25 pound bag of irresistible organic goodness for your cat",
+    price: 2000,
+    image: "kitty.jpg",
+    availableInventory: 10,
+    rating: 4,
+  },
+  {
+    id: 1002,
+    title: "Cat Litter",
+    description:
+      "A 25 pound bag of sand so your house doesn't smell like cat piss",
+    price: 3000,
+    image: "kitty2.jpg",
+    availableInventory: 10,
+    rating: 3,
+  },
+  {
+    id: 1003,
+    title: "Cat Clothes",
+    description:
+      "Raise shareef kids cover them up",
+    price: 2000,
+    image: "kitty3.jpg",
+    availableInventory: 10,
+    rating: 2,
+  },
+  {
+    id: 1004,
+    title: "Cat Weighing Machine",
+    description:
+      "Make sure your cat isnt fatter than you",
+    price: 2000,
+    image: "kitty4.jpg",
+    availableInventory: 10,
+    rating: 3,
+  },
+  {
+    id: 1005,
+    title: "Cat Carrier",
+    description:
+      "A 25 pound bag of <em>irresistible</em> organic goodness for your cat",
+    price: 2000,
+    image: "kitty5.jpg",
+    availableInventory: 10,
+    rating: 5,
+  },
+  {
+    id: 1006,
+    title: "Cat Weapon",
+    description:
+      "A 25 pound bag of <em>irresistible</em> organic goodness for your cat",
+    price: 2000,
+    image: "kitty6.jpg",
+    availableInventory: 10,
+    rating: 5,
+  },
+];
