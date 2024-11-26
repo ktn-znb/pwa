deb10d5a42f6f067b68db72e4e207a2e142d0a47
Zainab Khatoon <ktn.znb@gmail.com>
2024-11-26 16:09:28 +0400

    Added the event listener

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,13 @@ var cahceFiles = [
   "kitty5.jpg",
   "kitty6.jpg",
 ];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
