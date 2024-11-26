57fd069af3f498145d0b32e1e309bb1c9fca5e03
Zainab Khatoon <ktn.znb@gmail.com>
2024-11-26 16:03:21 +0400

    Adding the service worker file with cache name

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+var cacheName = "petstore";
+var cahceFiles = [
+  "week4_example_continued.html",
+  "product.js",
+  "petstore.webmanifest",
+  "image.png",
+  "kitty.jpg",
+  "kitty2.jpg",
+  "kitty3.jpg",
+  "kitty4.jpg",
+  "kitty5.jpg",
+  "kitty6.jpg",
+];
